0e41c17774582b5a682f52bd24984e505f8a6592
ddrous[macos] <rd.nzoyemngueguin@bristol.ac.uk>
2026-02-06 20:29:00 +0000

    Dataset done. Next, fix the dataloader and work on the Transformer

--- data_inspection.py
+++ data_inspection.py
@@ -20,6 +20,10 @@ for dirname, _, filenames in os.walk('/kaggle/input'):
 # import torch
 import matplotlib.pyplot as plt
 
+import seaborn as sns
+sns.set(style="white", context="talk")
+plt.rcParams['savefig.facecolor'] = 'white'
+
 
 # %%
 # train_path = "/kaggle/input/ts-forecasting/train.parquet"
@@ -163,7 +167,7 @@ df_train.groupby(["short_id","horizon"]).size().reset_index(name='counts').group
 nb_short_ids = len(df_train["short_id"].unique())
 dataset = {}
 
-for short_id in df_train["short_id"].unique()[:5]:
+for short_id in df_train["short_id"].unique()[:2]:
     dfs = []
     for horizon in [1,3,10,25]:
         df_sub = df_train[(df_train["short_id"]==short_id) & (df_train["horizon"]==horizon)]
@@ -172,7 +176,9 @@ for short_id in df_train["short_id"].unique()[:5]:
         idx = ts_index.argsort()
         df_sub = df_sub.iloc[idx]
         # Select from columns column 5 (ts_index) to column -2 (y_target)
-        df_sub = df_sub.iloc[:, 5:-2]
+        # df_sub = df_sub.iloc[:, 5:-2]
+
+        df_sub = df_sub.iloc[:, 5:]
         
         # Turn this into a numpy array
         arr = df_sub.to_numpy() 
@@ -183,4 +189,145 @@ for short_id in df_train["short_id"].unique()[:5]:
 dataset
 # %% [markdown]
 # Il faudra tout mettre dans un dataloader pour minimiser le temps d'entraînement
+# %%
+
+
+# len(dataset['W2MW3G2L__J0G2B0KU__PZ9S1Z4V'])
+
+dataset['W2MW3G2L__J0G2B0KU__PZ9S1Z4V'][0].shape
+
+
+
+
+
+
+#%% Create a complete pipleline for Pytorch dataloading, outputing time series of different lengths, with the same number of features, and the target variable.
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class HedgeFundDataset(Dataset):
+    def __init__(self, data_path, horizon=1, normalize=False):
+        self.df = pd.read_parquet(data_path)
+
+        ## Reorder the rows of the dataframe according to ts_index
+        self.df.sort_values(by=["ts_index"], inplace=True)
+
+        self.df["short_id"] = self.df["id"].apply(lambda x: remove_ts_suffix(x))
+
+        ## Get the mean and std of the features for normalization
+        feature_cols = [col for col in self.df.columns if col.startswith("feature_")]
+        self.feature_colds = feature_cols
+
+        if normalize:
+
+            self.df_features_mins = self.df[feature_cols].min()
+            self.df[feature_cols] = self.df[feature_cols] - self.df_features_mins + 1e-6
+
+            ## Tale the log of the features to make them more Gaussian
+            self.df[feature_cols] = np.log(self.df[feature_cols])
+
+            self.feature_means = self.df[feature_cols].mean()
+            self.feature_stds = self.df[feature_cols].std()
+
+            ## Normalize the features
+            self.df[feature_cols] = (self.df[feature_cols] - self.feature_means) / self.feature_stds
+
+        else:
+            self.feature_means = None
+            self.feature_stds = None
+            self.df_features_mins = None
+
+        self.horizon = horizon
+        self.short_ids = self.df["short_id"].unique()
+
+    def __len__(self):
+        return len(self.short_ids)
+
+    def __getitem__(self, idx):
+        short_id = self.short_ids[idx]
+
+        df_sub = self.df[(self.df["short_id"]==short_id) & (self.df["horizon"]==self.horizon)]
+        
+
+        ## Implement linear interpolation to fill the missing values of the time series
+        # df_sub = df_sub.set_index("ts_index").reindex(range(df_sub["ts_index"].min(), df_sub["ts_index"].max()+1)).interpolate(method="linear").reset_index()
+
+        ## Replace NaNs with mean of the feature for this short_id and this horizon
+        # df_sub[self.feature_colds].fillna(self.df[self.feature_colds].mean(), inplace=True)
+        
+
+        # print(df_sub["ts_index"])
+
+        # ts_index = df_sub["ts_index"].values
+        # indices = ts_index.argsort()
+        # df_sub = df_sub.iloc[indices]
+
+        df_sub = df_sub.iloc[:, 5:]
+        arr = df_sub.to_numpy(na_value=0.0)
+
+        # mean_datas = df_sub[self.feature_colds].mean().values
+        ## Convert nans to means
+        # arr = np.nan_to_num(arr, nan=0., posinf=0., neginf=mean_datas)
+
+        X_feats = arr[:, :-2]       ## Shape (T, D_x)
+        y_target = arr[:, -3:-2]    ## Shape (T, D_y)
+
+        # print("Types of Xfeats and y_target:", type(X_feats), type(y_target))
+
+        ## If t
+
+        return X_feats, y_target, df_sub["ts_index"].values
+
+train_dataset = HedgeFundDataset(train_path, horizon=25, normalize=True)
+iterator = iter(train_dataset)
+
+#%%
+## Create the dataloader
+train_dataloader = DataLoader(train_dataset, 
+                              batch_size=1, 
+                              shuffle=True, 
+                              num_workers=0)
+
+
+#%%
+
+count = 0
+feats_to_plot = np.random.choice(train_dataset[0][0].shape[1], size=2, replace=False)
+# feats_to_plot = [42, 55]
+
+i, j = feats_to_plot
+# for X, y, ts_ids in iterator:
+
+X, y, ts_ids = next(iterator)
+
+# for X, y, ts_ids in train_dataloader:
+
+# print("SHapes:", X.shape, y.shape)
+# print("SHapes:", X, y)
+## Plot the first few features and the target variable for the first time series in the dataset
+
+print(ts_ids)
+
+plt.figure(figsize=(10,5))
+plt.plot(ts_ids, X[:,i], label=f"Feature {i}") 
+plt.plot(ts_ids, X[:,j], label=f"Feature {j}")
+plt.plot(ts_ids, y[:,0], label="Target variable")
+plt.legend()
+
+# count += 1
+# if count == 2:
+#     break
+
+
+
+# (B, T_max, D_x) -> (B, T, D_y) 
+# %%
+
+train_dataset.df.head()
+
+# %%
+
+print(train_dataset.feature_means)
+
 # %%
